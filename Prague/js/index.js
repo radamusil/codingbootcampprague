7090b7fad2ee8ce4eb9683668be2ddd922350500
radamusil <64097743+radamusil@users.noreply.github.com>
2020-09-10 10:52:31 +0200

    First Days

---
+++ Prague/js/index.js
@@ -0,0 +1,7 @@
+let theButton = document.getElementById('theButton');
+let theText = document.querySelectorAll('.the-text');    
+theButton.onclick = function () { 
+    for(let x of theText) {
+        x.classList.toggle('colorized');
+    }
+}; 
